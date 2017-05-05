d868eebdaebacddad13eb2c65f458d3a7b0f531e
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-05-05 13:42:22 +0530

    profile

--- resources/views/venues/show.blade.php
+++ resources/views/venues/show.blade.php
@@ -314,9 +314,11 @@
 				<h3 class="desc-headline">Food Available</h3>
 				<ul class="property-features checkboxes margin-top-0">
 				 <?php $foods = json_decode($venue->food_available); ?>
+				 @if($foods != null)
 			      @foreach($foods as $item)
 					<li>{{ $item }}</li>
-				  @endforeach	
+				  @endforeach
+				 @endif 	
 				</ul>
                 
 
@@ -363,16 +365,16 @@
 
                 <h3 class="desc-headline">Venue Policies</h3>
 				<ul class="property-features terms margin-top-0">
-					<li>Liquor Allowed : {{ in_array("liquor_allowed", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' }}</li>
-					<li>DJ Allowed : {{ in_array("dj_allowed", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' }}</li>
+					<li>Liquor Allowed : {!! in_array("liquor_allowed", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' !!}</li>
+					<li>DJ Allowed : {!! in_array("dj_allowed", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' !!}</li>
 					@if(in_array("liquor_allowed", json_decode($venue->parameters)))
-					  <li>External Liquor : {{ in_array("external_liquor", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' }}</li>
+					  <li>External Liquor : {!! in_array("external_liquor", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">Not Allowed</span>' !!}</li>
 					@endif
-					<li>External Catering :{{ in_array("external_catering", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' }}</li>
+					<li>External Catering :{!! in_array("external_catering", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' !!}</li>
 					@if(in_array("dj_allowed", json_decode($venue->parameters)))
-					  <li>External DJ: {{ in_array("external_dj", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' }}</li>
+					  <li>External DJ: {!! in_array("external_dj", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' !!}</li>
 					@endif
-					<li>External Decorator : {{ in_array("external_decorator", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' }}</li>
+					<li>External Decorator : {!! in_array("external_decorator", json_decode($venue->parameters)) ? '<span>Allowed</span>' : '<span class="danger">From Panel</span>' !!}</li>
 				    @if($venue->rooms > 0)
 					<li>Rooms : <span class="danger">Charged Seperately</span></li>
 					@endif
