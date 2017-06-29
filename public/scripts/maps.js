0007f1ea8945f378ddb52ac66d64467b8d2fa542
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-06-29 12:07:33 +0530

    Beta Stage 1

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,4 +1,19 @@
- map = new google.maps.Map(document.getElementById('map'), {
+(function($) {
+    "use strict";
+    
+    $(window).on('load resize', function() {
+        var topbarHeight=$("#top-bar").height();
+        var headerHeight=$("#header").innerHeight()+ topbarHeight;
+        $(".fs-container").css('height', ''+ $(window).height()- headerHeight+'px');
+
+         map = new google.maps.Map(document.getElementById('propertyMap'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
+    }
+    );
+   
+    
+}
+
+)(this.jQuery);
